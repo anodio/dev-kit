623cc856226317ee576be1cf6e158d33dec9100a
Vladimir <vladimir@sobaka.com>
2024-08-15 15:57:59 +0200

    init2

--- src/DevPack.php
+++ src/DevPack.php
@@ -1,6 +1,35 @@
 <?php
+namespace Anodio\DevKit;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class DevPack
+#[\Bicycle\Core\Attributes\Command('dev:pack', 'Move package from vendor for development')]
+class DevPack extends Command
 {
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
 
+        $vendorDir = BASE_PATH . '/vendor';
+        $packageName = $input->getArgument('packageName');
+        $composerJsonPath = $vendorDir.'/'.$packageName.'/composer.json';
+        $composerJson = json_decode(file_get_contents($composerJsonPath), true);
+
+        $httpGitAddress = $composerJson['extra']['dev-kit']['git']['https'];
+        $sshGitAddress = $composerJson['extra']['dev-kit']['git']['ssh'];
+
+        //now lets clone package to another dir
+
+        @mkdir(BASE_PATH.'/'.$packageName);
+        shell_exec('git clone '.$httpGitAddress.' '.BASE_PATH.'/'.$packageName);
+        shell_exec('cd '.BASE_PATH.'/'.$packageName.' && git remote set-url origin '.$sshGitAddress);
+
+        return 0;
+    }
+
+    protected function configure()
+    {
+        $this->addArgument('packageName', InputArgument::REQUIRED, 'package name');
+    }
 }
